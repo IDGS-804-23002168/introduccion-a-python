0b9357b988daf519a74e67a5fc420ee768019d71
IDGS-804-23002168 <82916@alumnos.utleon.edu.mx>
2026-01-16 18:31:52 -0600

    Funciones

---
+++ 04-while.py
@@ -0,0 +1,13 @@
+# Se me habia modificado y cuando quise subir el sig, commit
+# Por querer que no se subiera en los nuevos cambios
+# Se me borro del repositorio, asi que se lo subo otra vez
+
+x = 0
+tem = ''
+
+while x < 5:
+    tem += str(x) + '+'
+    x += 2
+
+print("La suma de los numeros pares es menor a 10")
+print(tem)
